2c8b67a860f13e2b2caa149379c9234ffbc08b4a
Jubaer Hossen <jobaer1618@gmail.com>
2023-09-24 21:59:20 +0600

    added nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+   theme: {
+      extend: {},
+   },
+   plugins: [require("daisyui")],
+   daisyui: {
+      themes: [
+         {
+            myTheme: {
+               primary: "#ff6600",
+               secondary: "#ddd",
+               accent: "#b4b4b4",
+               neutral: "#8c8c8c",
+               someDeep: "#3d3d3d",
+               "base-100": "#ffffff",
+            },
+         },
+      ],
+   },
 };
